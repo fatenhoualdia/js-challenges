2e08492c3582b42cf3bac5d5b5794017966acdf2
faten <fatenkhoualdia@gmail.com>
2023-11-21 16:49:22 +0100

    fix sign up

--- 03-dom-challenges/toggle_password/script.js
+++ 03-dom-challenges/toggle_password/script.js
@@ -15,3 +15,17 @@ toggleButton.addEventListener('click', function () {
     toggleButton.alt = 'Show Password'
   }
 });
+// Get the elements
+const signUpCheckbox = document.getElementById("sign-up");
+const additionalFields = document.getElementById("additional-fields");
+
+// Event listener for the "Sign Up" checkbox
+signUpCheckbox.addEventListener("change", function() {
+    if (this.checked) {
+        // Show additional fields when checkbox is checked
+        additionalFields.style.display = "block";
+    } else {
+        // Hide additional fields when checkbox is unchecked
+        additionalFields.style.display = "none";
+    }
+});
